d0d7af23f9cd996096b45b7fcec74bad18d8f087
Augusto Melo <4723788+augustomelo@users.noreply.github.com>
2025-05-01 21:38:30 +0100

    feat: add test for response codes

--- app/routers/hello.py
+++ app/routers/hello.py
@@ -1,7 +1,10 @@
 import logging
+from datetime import datetime
 from typing import Annotated
 
-from fastapi import APIRouter, Body, status
+from fastapi import APIRouter, Body, HTTPException, status
+
+from app.internal.validators import is_dob_valid, is_username_valid
 
 router = APIRouter()
 
@@ -9,8 +12,29 @@ logger = logging.getLogger(__name__)
 
 
 @router.put("/{username}", status_code=status.HTTP_204_NO_CONTENT)
-async def upsert_birthday(username: str, dob: Annotated[str, Body(alias="dateOfBirth", embed=True)]):
-    logger.info(f"Received username={username} dateOfBirth={dob}")
+async def upsert_birthday(
+    username: str, dobstr: Annotated[str, Body(alias="dateOfBirth", embed=True)]
+):
+    logger.info(f"Received username={username} dateOfBirth={dobstr}")
+
+    if not is_username_valid(username):
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, detail="Invalid username"
+        )
+
+    try:
+        dob = datetime.strptime(dobstr, "%Y-%m-%d").date()
+    except ValueError:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Ivalid dateOfBirth format, corret one is: 'YYYY-MM-DD'",
+        )
+
+    if not is_dob_valid(dob):
+        raise HTTPException(
+            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+            detail="Invalid dateOfBirth value, it must be a date before the today date",
+        )
 
 
 @router.get("/{username}")
--- tests/integration/test_hello.py
+++ tests/integration/test_hello.py
@@ -1,4 +1,5 @@
 import pytest
+from fastapi import status
 from fastapi.testclient import TestClient
 
 from app.main import app
@@ -8,13 +9,35 @@ from app.main import app
 def test_client():
     return TestClient(app)
 
-def test_upsert_birthday(test_client: TestClient):
-    resp = test_client.put("/hello/augusto", json={"dateOfBirth": "1991-08-06"})
 
-    assert resp.status_code == 204
+def test_upsert_birthday(test_client: TestClient):
+    resp = test_client.put("/hello/augusto", json={"dateOfBirth": "1992-08-06"})
+
+    assert resp.status_code == status.HTTP_204_NO_CONTENT
+
+
+def test_invalid_username_upsert_birthday(test_client: TestClient):
+    resp = test_client.put(
+        "/hello/invalid-username", json={"dateOfBirth": "1992-08-06"}
+    )
+
+    assert resp.status_code == status.HTTP_400_BAD_REQUEST
+
+
+def test_invalid_dob_format_upsert_birthday(test_client: TestClient):
+    resp = test_client.put("/hello/augusto", json={"dateOfBirth": "06-08-1992"})
+
+    assert resp.status_code == status.HTTP_400_BAD_REQUEST
+
+
+def test_invalid_dob_future_upsert_birthday(test_client: TestClient):
+    resp = test_client.put("/hello/augusto", json={"dateOfBirth": "3000-08-02"})
+
+    assert resp.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
+
 
 def test_get_birthday(test_client: TestClient):
     resp = test_client.get("/hello/augusto")
 
-    assert resp.status_code == 200
-    assert (resp.json() is not None and resp.json() != "")
+    assert resp.status_code == status.HTTP_200_OK
+    assert resp.json() is not None and resp.json() != ""
