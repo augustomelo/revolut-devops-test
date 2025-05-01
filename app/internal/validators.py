e51fdd5e70e0a33f74347ebe9d65523ff76caff8
Augusto Melo <4723788+augustomelo@users.noreply.github.com>
2025-05-01 20:09:09 +0100

    feat: add date validation

--- app/internal/validators.py
+++ app/internal/validators.py
@@ -1,10 +1,23 @@
 import re
+from datetime import date, datetime
 
 _pattern_letters = re.compile("^[a-zA-Z]+$")
 
-def is_username_valid(username: str) -> bool:
 
+def is_username_valid(username: str) -> bool:
     if _pattern_letters.match(username):
         return True
-    
+
     return False
+
+
+def is_dob_valid(dob: date) -> bool:
+    # must be a date before the today date
+    if dob < get_today():
+        return True
+
+    return False
+
+
+def get_today() -> date:
+    return datetime.now().date()
--- tests/unit/test_validators.py
+++ tests/unit/test_validators.py
@@ -1,8 +1,35 @@
-from app.internal.validators import is_username_valid
+from datetime import datetime
+
+from pytest_mock import MockerFixture
+
+from app.internal.validators import is_dob_valid, is_username_valid
 
 
 def test_valid_username_is_username_valid():
-    assert is_username_valid("username") is True
+    assert is_username_valid("username")
+
 
 def test_invalid_username_is_username_valid():
-    assert is_username_valid("") is False
+    assert not is_username_valid("")
+
+
+def test_before_dob_is_dob_valid(mocker: MockerFixture):
+    mock_date = mocker.patch("app.internal.validators.get_today")
+    mock_date.return_value = datetime(2025, 4, 1).date()
+
+    assert is_dob_valid(datetime(2025, 3, 30).date())
+
+
+# must be a date before the today date
+def test_equal_dob_is_dob_valid(mocker: MockerFixture):
+    mock_date = mocker.patch("app.internal.validators.get_today")
+    mock_date.return_value = datetime(2025, 4, 1).date()
+
+    assert not is_dob_valid(datetime(2025, 4, 1).date())
+
+
+def test_after_dob_is_dob_valid(mocker: MockerFixture):
+    mock_date = mocker.patch("app.internal.validators.get_today")
+    mock_date.return_value = datetime(2025, 4, 1).date()
+
+    assert not is_dob_valid(datetime(2025, 4, 2).date())
